4d429e2539fcc8af4260c6d91c187d83e8ef60bd
Al <Al@24.240.107.67>
2014-12-13 10:50:38 -0600

    Just got hooked into Git, trying to upload. Added do nothing code to Attack class, perform method to test.

--- src/engine/Attack.java
+++ src/engine/Attack.java
@@ -5,6 +5,10 @@ public class Attack {
 	public void perform()
 	{
 		// TODO: Stuff
+		
+		//practice uploading content
+		double kick = 15.23;
+		System.out.printf("Do jump kicks and stuff! %.2f", kick);
 	}
 	
 	public Integer getDamage()
