e4015370993fc75abcd5cecbb84acf325b0af4f2
TinyTom99 <trismeyer@gmail.com>
2015-03-03 21:37:45 -0600

    Forgotten Changes
    Forgot to upload these changes earlier

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -45,30 +45,26 @@ public class MainWindow {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Adventure");
 		btnNewButton.setBounds(50, 187, 800, 167);
 		frame.getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		JButton btnNewButton_2 = new JButton("2-Player");
 		btnNewButton_2.setBounds(50, 364, 800, 167);
 		frame.getContentPane().add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("New button");
+		JButton btnNewButton_3 = new JButton("Credits");
 		btnNewButton_3.setBounds(50, 541, 800, 167);
 		frame.getContentPane().add(btnNewButton_3);
 		
-		JLabel label = new JLabel("New label");
+		JLabel label = new JLabel("Eternal Conflict");
 		label.setBounds(50, 10, 800, 167);
 		frame.getContentPane().add(label);
 		
 		Image plane = Toolkit.getDefaultToolkit().getImage("PossiblyTheBestTitlePhraseEver.png");
 		BackgroundPanel panel = new BackgroundPanel(plane,BackgroundPanel.ACTUAL);
 		panel.setLayout(null);
-		panel.setBounds(23, 76, 30, 100);
+		panel.setBounds(0, 0, 884, 729);
 		frame.getContentPane().add(panel);
-		
-		JCheckBox chckbxHi = new JCheckBox("hi");
-		chckbxHi.setBounds(6, 44, 97, 23);
-		panel.add(chckbxHi);
 	}
 }
